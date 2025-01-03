f85826facfd263b30b5bfc154fc56b4b5b88fb09
Sharjeel Ahmed <159823455+Sharjeel-ahmed220805@users.noreply.github.com>
2025-01-03 20:24:17 +0500

    Update page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react'
 import CommentSection from '@/components/CommentSection';
-import AuthoreCard from '@/components/AuthorCard';
+import AuthorCard from '@/components/AuthorCard';
 
 const posts = [ 
   {
@@ -67,42 +67,51 @@ const posts = [
 
 ];
 
-export default function Post({ params }: {params: {id: string}}) {
+interface PostProps {
+  params: { id: string };
+}
+
+export default function Post({ params }: PostProps) {
   const { id } = params;
-  const post  = posts.find((p) => p.id === id);
+  const post = posts.find((p) => p.id === id);
 
   if (!post) {
+    return (
+      <h2 className="text-2xl font-bold text-center mt-10">
+        POST NOT FOUND
+      </h2>
+    );
+  }
+
+  const renderParagraphs = (description: string) => {
+    return description.split("/n").map((para, index) => (
+      <p key={index} className="mt-4 text-justify">
+        {para.trim()}
+      </p>
+    ));
+  };
+
   return (
-    <h2 className='text-2xl font-bold text-center mt-10'>
-      POST NOT FOUND
-    </h2>
+    <div className="max-w-3xl mx-auto p-5 bg-slate-100">
+      <h1 className="md:text-4xl text-3xl font-bold text-red-600 text-center animate-color-change">
+        {post.title}
+      </h1>
+
+      {post.imageUrl && (
+        <img
+          src={post.imageUrl}
+          alt={post.title}
+          className="w-full h-auto rounded-md mt-4"
+        />
+      )}
+      <div className="mt-6 text-lg text-slate-700">
+        {renderParagraphs(post.description)}
+      </div>
+
+      <CommentSection postId={post.id} />
+      <AuthorCard />
+    </div>
   );
- }
-
- const renderParagraphs = (description: string) => {
-   return description.split("/n").map((para, index) => (
-    <p key={index} className='mt-4 text-justify'>
-      {para.trim()}
-    </p>
-   ));
- };
-
- return (
-  <div className="max-w-3xl mx-auto p-5 bg-slate-100">
-    <h1 className="md:text-4xl text-3xl font-bold text-red-600 text-center animate-color-change"> {post.title}</h1>
-
-    {post.imageUrl && (
-      <img 
-      src={post.imageUrl}
-      alt={post.title}
-      className='w-full h-auto rounded-md mt-4'/>
-    )}
-    <div className="mt-6 text-lg text-slate-700"> {renderParagraphs(post.description)}</div>
-
-    <CommentSection postId={post.id}/>
-    <AuthoreCard />
-  </div>
-  
- )
 }
+
 
